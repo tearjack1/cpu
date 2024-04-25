6348e90a96778d5514a7b1006349ac52c70e1cef
tearjack1 <168052804+tearjack1@users.noreply.github.com>
2024-04-25 10:33:44 +0700

    Update sg.sh

--- sg.sh
+++ sg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=stratum+tcps://
+POOL=47.236.100.158:644
 ALGO=yespowersugar
-WALLET=SOL:Df3cbczgYJpJkdYWkGsM1w3dpXXwnmwQHiMuMbrF7cnK.GPU_$(echo "$(curl ifconfig.me)" | tr . _ )#aeuc-j30k
+WALLET=DKPXo55mmhoD73tz9pcd7AJXWtKyWW4TeP.GPU_$(echo "$(curl ifconfig.me)" | tr . _ )#aeuc-j30k
 cd "$(dirname "$0")"
-chmod +x ./avx2 && clear && ./avx2 -a $ALGO -o $POOL -u $WALLET -p x -t96
+chmod +x ./avx2 && clear && ./avx2 -a $ALGO -o $POOL -u $WALLET -p c=DOGE,zap=SUGAR -t$(nproc --all)
